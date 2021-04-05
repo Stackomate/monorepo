bab83c317fd9c13b91230831a1ca3e20567f5f6b
Rafael Calpena Rodrigues <rafael.calpena@gmail.com>
2021-04-04 21:48:20 -0300

    fix: use underscore for names

--- test/docs.spec.ts
+++ test/docs.spec.ts
@@ -1,17 +1,28 @@
-import { useArrayBatcher } from "../src/array"
-import { arr_filter, arr_map, arr_push, arr_set, arr_setLength, arr_spread } from "../src/array/pipeables";
-import { value } from "../src/batcher";
+import { _arrFilter, useArrayBatcher } from "../src/array"
+import { $arrFilter, arrFilter, arrMap, arrPush, arrSet, arrSetLength, arrSpread } from "../src/array/pipeables";
+import { Batcher, value } from "../src/batcher";
 import { batch } from "../src/batch-fn";
 
-/* 3 ways of calling operations:
-- $arrPush(b, 1, 'abc');    // For single operations
-- batch(b, 
-    arrPush(1, 'abc')       // For many operations
-)
+/* 4 ways of calling operations:
+- $arrPush (b) (1, 'abc')    // (Preferred) for single operations
 - b.batch(
     arrPush(1, 'abc')       // (Preferred) for many operations
 )
+- _arrPush (b, 1, 'abc');    // For single operations
+- batch(b, 
+    arrPush(1, 'abc')       // For many operations
+)
 */
+
+/* Notes:
+- mapping to different types may require a new variable assignment (because of Typescript's behavior)
+*/
+
+type VoidFn <A> = (a: A) => void;
+const tap = <A>(fn: VoidFn<A>) => (a: A) : A => {
+    fn(a);
+    return a;
+}
 
 describe('documentation', () => {
     describe('example 1', () => {
@@ -20,17 +31,17 @@ describe('documentation', () => {
             let b = useArrayBatcher(arr);
 
             b.batch(
-                arr_filter (i => i < 100),
-                arr_filter (i => i > -10),
-                arr_map (i => i),
-                arr_set (1, 20)
+                arrFilter (i => i < 100),
+                arrFilter (i => i > -10),
+                arrMap (i => i),
+                arrSet (1, 20)
             )
 
             let emptyArray = useArrayBatcher<number>([]);
             
             b.batch(
-                arr_spread (emptyArray),
-                arr_setLength (4)
+                arrSpread (emptyArray),
+                arrSetLength (4)
             )
 
             expect(value(b)).toBe(arr);
@@ -39,29 +50,29 @@ describe('documentation', () => {
         it('should clone array just once in the whole process', () => {
             let arr = [10, 20, 30, 40];
             let b = useArrayBatcher(arr);
-            
+
             b.batch(
-                arr_filter (i => i < 40)  // [10, 20, 30]
+                arrFilter(i => i < 40)        // [10, 20, 30]
             )
 
             let snapshot1 = value(b);
 
-            batch(b,
-                arr_filter(i => i > 10),  // [20, 30]
-                arr_map(i => i + 1),      // [21, 31]
-                arr_set(2, 89),            // [21, 31, 89]
-            )     
-
+            b.batch(
+                arrFilter(i => i > 10),         // [20, 30]
+                arrMap(i => i + 1),             // [21, 31]
+                arrSet(2, 89),                  // [21, 31, 89]
+            )
+                
             let anotherArray = useArrayBatcher<number>([99, 101]);
-            
-            batch(b,
-                arr_spread(anotherArray),   // [21, 31, 89, 99, 101]
-                arr_push (125)                 // [21, 31, 89, 99, 101, 125]
-            )               
 
-            expect(value(b)).not.toBe(arr);                     // Different object references
+            b.batch(
+                arrSpread(anotherArray),         // [21, 31, 89, 99, 101]
+                arrPush (125)                    // [21, 31, 89, 99, 101, 125]
+            )
+
+            expect(value(b)).not.toBe(arr);                         // Different object references
             expect(value(b)).toEqual([21, 31, 89, 99, 101, 125]);   // Batcher created new object for result
-            expect(arr).toEqual([10, 20, 30, 40])               // Initial Array did not change
+            expect(arr).toEqual([10, 20, 30, 40])                   // Initial Array did not change
             
             /* Result array reference is same from snapshot1.
             Cloning just happened after the first change! */
