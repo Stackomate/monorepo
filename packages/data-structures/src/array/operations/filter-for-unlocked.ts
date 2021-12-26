cd00dbd100a433688a7a2d575d282ba1e16c2522
Rafael Calpena Rodrigues <rafael.calpena@gmail.com>
2021-12-26 17:16:09 -0300

    chore: micro optimization

--- packages/data-structures/src/array/operations/filter-for-unlocked.ts
+++ packages/data-structures/src/array/operations/filter-for-unlocked.ts
@@ -2,11 +2,14 @@ import { Batcher } from '../../batcher';
 import { _arrForEach } from "../queries/_arrForEach";
 import { _arrSetLength } from "./_arrSetLength";
 
-/* This is a protected function, therefore should not be exposed in the public api */
+/* Internal function, should not be exposed in the public api */
 export const arrFilterForUnlocked = <T>(batcher: Batcher<Array<T>>, fn: (a: T) => boolean): Batcher<Array<T>> => {
     let deletions = 0;
     _arrForEach(batcher, (item, index, arr) => {
-        arr[index - deletions] = item;        
+        if (deletions > 0) {
+            arr[index - deletions] = item; 
+        }
+
         if (!fn(item)) {
             deletions++;
         }
