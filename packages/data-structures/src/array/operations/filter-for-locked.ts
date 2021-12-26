94210a5bab310163c212a9f206c5b929b1f70fa3
Rafael Calpena Rodrigues <rafael.calpena@gmail.com>
2021-12-26 17:19:38 -0300

    chore: update filter-for-locked

--- packages/data-structures/src/array/operations/filter-for-locked.ts
+++ packages/data-structures/src/array/operations/filter-for-locked.ts
@@ -2,18 +2,16 @@ import { Batcher } from '../../batcher';
 import { _arrForEach } from "../queries/_arrForEach";
 
 export function arrFilterForLocked<T>(batcher: Batcher<T[]>, fn: (a: T) => boolean) {
-    const result: Array<T> = [];
     let hasChanged = false;
-    _arrForEach(batcher, item => {
-        if (fn(item)) {
-            result.push(item);
-        } else {
+    _arrForEach(batcher, (item, index) => {
+        let isValidItem = fn(item);
+        if (!isValidItem && !hasChanged) {
+            /* Only clone array until previous item */
+            batcher.willChange({to: index - 1});
             hasChanged = true;
+        } else if (isValidItem && hasChanged) {
+            batcher.currentValue.push(item);
         }
     });
-    if (hasChanged) {
-        batcher.willChangeWithoutCloning();
-        batcher.currentValue = result;
-    }
     return batcher;
 }
